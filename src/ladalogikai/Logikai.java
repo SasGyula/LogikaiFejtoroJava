73d4b14f176e628b3614cba1dc337dc894b84956
Lajkó Gege <127200655+lajkogege@users.noreply.github.com>
2024-09-11 14:56:41 +0200

    guikesz

---
+++ src/ladalogikai/Logikai.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ladalogikai;
+
+/**
+ *
+ * @author LajkóGergő(SZF_2023_
+ */
+public class Logikai {
+    
+}
